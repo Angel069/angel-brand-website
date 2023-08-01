a34ab5aa5d383e96f58c3464ecd37549800eb8ff
Angel Brand <angel.brand062@gmail.com>
2023-08-01 13:38:39 -0600

    add responsive styles and aos.js

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+AOS.init();
+
+AOS.init ({
+    duration: 1500,
+})
+
+window.onload = function () {
+    lax.init()
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+      return window.scrollY
+    })
+
+    // Add animation bindings to elements
+    lax.addElements('.selector', {
+      scrollY: {
+        translateX: [
+          ["elInY", "elCenterY", "elOutY"],
+          [0, 'screenWidth/2', 'screenWidth'],
+        ]
+      }
+    })
+  }
+
